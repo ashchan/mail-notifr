5c05836224cd2f6fb2f41cdce16063a9f38d58de
James Chen <ashchan@gmail.com>
2014-04-17 21:05:47 +0900

    Add shortcut (CMD+V) for pasting password
    Close #30

---
+++ Classes/GNSecureTextField.h
@@ -0,0 +1,13 @@
+//
+//  GNSecureTextField.h
+//  Mail Notifr
+//
+//  Created by James Chen on 4/17/14.
+//  Copyright (c) 2014 ashchan.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GNSecureTextField : NSSecureTextField
+
+@end
---
+++ Classes/GNSecureTextField.m
@@ -0,0 +1,23 @@
+//
+//  GNSecureTextField.m
+//  Mail Notifr
+//
+//  Created by James Chen on 4/17/14.
+//  Copyright (c) 2014 ashchan.com. All rights reserved.
+//
+
+#import "GNSecureTextField.h"
+
+@implementation GNSecureTextField
+
+- (BOOL)performKeyEquivalent:(NSEvent *)event {
+    NSUInteger flags = [event modifierFlags] & NSDeviceIndependentModifierFlagsMask;
+    NSString *key = [event charactersIgnoringModifiers];
+    if (flags == NSCommandKeyMask && [key isEqualToString:@"v"]) {
+        return [NSApp sendAction:@selector(paste:) to:[self.window firstResponder] from:self];
+    }
+
+    return NO;
+}
+
+@end
