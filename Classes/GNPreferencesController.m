fd7806d5716a0ccec211336be6078938694dec8e
James Chen <james@ashchan.com>
2021-06-12 11:44:59 +0900

    Update preferences window layout

--- Classes/GNPreferencesController.m
+++ Classes/GNPreferencesController.m
@@ -13,20 +13,6 @@
 #import "PrefsInfoViewController.h"
 #import "GNPreferences.h"
 
-@interface GNToolbarFlexibleModule : NSObject
-
-@property (copy, readonly) NSString *identifier;
-
-@end
-
-@implementation GNToolbarFlexibleModule
-
-- (NSString *)identifier {
-    return NSToolbarFlexibleSpaceItemIdentifier;
-}
-
-@end
-
 @interface GNPreferencesController () <NSToolbarDelegate, NSWindowDelegate>
 
 @property (strong) id eventMonitor;
@@ -45,7 +31,6 @@
         controller.modules = @[
             [[PrefsAccountsViewController alloc] init],
             [[PrefsSettingsViewController alloc] init],
-            [[GNToolbarFlexibleModule alloc] init],
             [[PrefsInfoViewController alloc] init]
         ];
     });
