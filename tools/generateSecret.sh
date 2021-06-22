b119e8699e5613b9d3283cedd587621d43bd2b16
James Chen <james@ashchan.com>
2021-06-22 21:10:06 +0900

    Move generating secret from gyb template to pre-action
    Build phase for updating source files makes SwiftUI preview fail to build.

---
+++ tools/generateSecret.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ -e "${PROJECT_DIR}/.env" ]]; then
+    source "${PROJECT_DIR}/.env"
+fi
+find "${PROJECT_DIR}" -name '*.gyb' |
+  while read file; do
+    if [[ ! -f "${file%.gyb}" ]]; then
+        echo "Generating ${file%.gyb}";
+        "${PROJECT_DIR}/tools/gyb" --line-directive '' -o "${file%.gyb}" "$file";
+    fi
+  done
+
