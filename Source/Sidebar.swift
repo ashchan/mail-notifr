c1b7fa70cbcc0adbf0e0dc126679596ea45597d1
James Chen <james@ashchan.com>
2021-10-30 21:52:51 +0900

    Drag and drop account to reorder

--- Source/Sidebar.swift
+++ Source/Sidebar.swift
@@ -31,6 +31,13 @@ struct Sidebar: View {
                     }
                     .padding(2)
                 }
+                .onMove { source, destination in
+                    let email = accounts[source.first!].email
+                    accounts.reorder(fromOffsets: source, toOffset: destination)
+                    DispatchQueue.main.async {
+                        selection = email
+                    }
+                }
 
                 Text("Preferences")
                     .font(.subheadline)
